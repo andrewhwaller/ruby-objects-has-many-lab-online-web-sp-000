f3b8370ab94ee73b11da3f2f4ee55d06a260b4d0
Andrew Waller <andrewhwaller@gmail.com>
2019-03-21 21:59:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,14 @@
 class Artist
   attr_accessor :name
 
-  @songs = []
-
   def initialize(name)
     @name = name
+    @songs = []
   end
 
   def add_song_by_name(song_name)
-    
+
+  end
 
   def self.song_count
     @songs
